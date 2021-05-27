a4438f8d8312bf8e9fdd3f84929cbbc25c370c1e
Cindy Liang <hakkacindy@gmail.com>
2021-05-27 23:44:23 +0100

    friend create method added

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,28 @@
 class FriendsController < ApplicationController
+  def index
+    @friends = Friend.all
+  end
+
+  def show
+    @friend = Friend.find(params[:id])
+
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+    @friend.user = current_user
+    if @friend.save
+      redirect_to friends_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  
+  def friend_params
+    params.require(:friend).permit(:name, :user_id)
+  end
+
 end
