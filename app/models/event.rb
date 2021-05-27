480bda8757ded8211b80ba7622ff7d663ca34455
Emily Giblett <elgiblett@gmail.com>
2021-05-27 20:21:25 +0100

    added validations to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :friend
+  validates :event_type, presence: true
+  validates :future_date, presence: true
+
+  def future_date
+    errors.add(:date, "Cannot be in the past") if date < Date.today
+  end
 end
