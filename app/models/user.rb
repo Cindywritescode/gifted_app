420f8c0acce8790c00d045a555b607dc3f7e7f8a
Thomas Loubser <taloubser@gmail.com>
2021-05-27 20:10:50 +0100

    added associations to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :gifts
+  has_many :friends
+  has_many :notes
 end
