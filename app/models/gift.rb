6e188814add96132649fac31ffb8e0acdbbd5f6f
Dan-Mog <60817382+Dan-Mog@users.noreply.github.com>
2021-05-27 20:28:53 +0100

    Update gift.rb

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,6 @@
 class Gift < ApplicationRecord
   belongs_to :user
+  has_many :gift_ideas, dependent: :destroy
+  has_many :friends, through: :gift_ideas
   validates :gift_url, presence: true
 end
