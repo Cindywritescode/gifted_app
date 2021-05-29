10594c7dde5a2963fc4d63a8c0b60515dd445bb9
Emily Giblett <elgiblett@gmail.com>
2021-05-29 11:36:39 +0100

    added name column to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# require 'faker'
+
 puts "Destroying all Users, gifts, friends, events, notes & gift ideas..."
 puts "Destroying all Users...."
 User.destroy_all
@@ -11,3 +13,18 @@ puts "Destroying all Notes...."
 Note.destroy_all
 puts "Creating Users, gifts, friends, events, notes & gift ideas..."
 puts "--"
+
+emily = User.new(
+  name: "Emily",
+  email: "emily@me.org",
+  password: "password"
+)
+emily.save!
+puts "User created: #{emily.name}"
+
+friend1 = Friend.new(
+  name: "#{Faker::Name.name}",
+  user_id: emily.id
+  )
+friend1.save!
+puts "Friend created: #{friend1.name}"
