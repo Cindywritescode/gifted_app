3da4e9037504535cd95eb42070764ee9d6eea9fa
Emily Giblett <elgiblett@gmail.com>
2021-05-29 14:35:40 +0100

    seedfile created

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :friend
   validates :event_type, presence: true
-  validates :date, presence: true
+  validate :future_date
 
   def future_date
     errors.add(:date, "Cannot be in the past") if date < Date.today
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
-# require 'faker'
-
 puts "Destroying all Users, gifts, friends, events, notes & gift ideas..."
 puts "Destroying all Gifts...."
 Gift.destroy_all
 puts "Destroying all Events...."
 Event.destroy_all
-puts "Destroying all Friends (bye bitches 😉) ...."
-Friend.destroy_all
 puts "Destroying all Notes...."
 Note.destroy_all
+puts "Destroying all Friends (bye bitches 😉) ...."
+Friend.destroy_all
 puts "Destroying all Users...."
 User.destroy_all
 puts "Creating Users, friends, events, gifts, notes & gift ideas..."
@@ -78,22 +76,177 @@ gift3 = Gift.new(
   price: (35..70).to_a.sample,
   user_id: emily.id
   )
-# gift2.photo.attach(io: File.open(File.join(Rails.root,'app/assets/images/seed/products/adopt.jpg')), filename: 'adopt.jpg')
+# gift3.photo.attach(io: File.open(File.join(Rails.root,'app/assets/images/seed/products/adopt.jpg')), filename: 'adopt.jpg')
 gift3.save!
 puts "Gift created: #{gift3.product_name}"
 
+note1 = Note.new(
+  content: "I remember #{friend1.id} saying that they love penguins",
+  friend_id: friend1.id,
+  user_id: emily.id
+  )
+note1.save!
+
+note2 = Note.new(
+  content: "#{friend2.id} thought the #{gift2.product_name} was really cute",
+  friend_id: friend2.id,
+  user_id: emily.id
+  )
+note2.save!
+
+note3 = Note.new(
+  content: "#{friend1.id} is a skincare fanatic",
+  friend_id: friend1.id,
+  user_id: emily.id
+  )
+note3.save!
+
+puts "3 notes created"
+
+gift_idea1 = GiftIdea.new(
+  friend_id: friend1.id,
+  gift_id: gift1.id
+  )
+gift_idea1.save!
+
+gift_idea2 = GiftIdea.new(
+  friend_id: friend2.id,
+  gift_id: gift3.id
+  )
+gift_idea2.save!
+gift_idea3 = GiftIdea.new(
+  friend_id: friend1.id,
+  gift_id: gift2.id
+  )
+gift_idea3.save!
+puts "Gift ideas added to friends"
+
+puts "--"
+
+cindy = User.new(
+  name: "Cindy",
+  email: "cindy@me.org",
+  password: "password"
+)
+cindy.save!
+puts "User created: #{cindy.name}"
+
+friend3 = Friend.new(
+  name: "#{Faker::Name.name}",
+  user_id: cindy.id
+  )
+friend3.save!
+puts "Friend created: #{friend3.name}"
+
+friend4 = Friend.new(
+  name: "#{Faker::Name.name}",
+  user_id: cindy.id
+  )
+friend4.save!
+puts "Friend created: #{friend4.name}"
+
+event3 = Event.new(
+  event_type: "Baby Shower",
+  date: Date.parse('2021-07-15'),
+  friend_id: friend3.id
+  )
+event3.save!
+puts "Event created: #{friend3.name}'s #{event3.event_type}"
+
+event4 = Event.new(
+  event_type: "Graduation",
+  date: Date.parse('2021-08-27'),
+  friend_id: friend4.id
+  )
+event4.save!
+puts "Event created: #{friend4.name}'s #{event4.event_type}"
+
+gift3 = Gift.new(
+  gift_url: "https://www.spacenk.com/uk/fragrance/home-fragrance/spray-diffuser/post-poo-drops",
+  product_name: "Aesop Post Poo Drops",
+  price: (15..70).to_a.sample,
+  user_id: cindy.id
+  )
+# gift3.photo.attach(io: File.open(File.join(Rails.root,'app/assets/images/seed/products/poodrops.jpg')), filename: 'poodrops.jpg')
+gift3.save!
+puts "Gift created: #{gift3.product_name}"
+
+gift4 = Gift.new(
+  gift_url: "https://www.buyagift.co.uk/flying-experiences/sunrise-hot-air-balloon-ride",
+  product_name: "Hot Air Balloon Ride",
+  price: (35..70).to_a.sample,
+  user_id: cindy.id
+  )
+# gift4.photo.attach(io: File.open(File.join(Rails.root,'app/assets/images/seed/products/hotairballoon.jpg')), filename: 'hotairballoon.jpg')
+gift4.save!
+puts "Gift created: #{gift4.product_name}"
+
+gift5 = Gift.new(
+  gift_url: "https://www.amazon.co.uk/Untitled-Animal-Unisex-Casual-Novelty/dp/B082FBWT8Y/",
+  product_name: "Goose Socks",
+  price: (5..70).to_a.sample,
+  user_id: cindy.id
+  )
+# gift5.photo.attach(io: File.open(File.join(Rails.root,'app/assets/images/seed/products/adopt.jpg')), filename: 'adopt.jpg')
+gift5.save!
+puts "Gift created: #{gift5.product_name}"
+
+note4 = Note.new(
+  content: "I remember #{friend3.id} saying that they love penguins",
+  friend_id: friend3.id,
+  user_id: cindy.id
+  )
+note4.save!
+
+note5 = Note.new(
+  content: "#{friend4.id} thought the #{gift3.product_name} was really cute",
+  friend_id: friend4.id,
+  user_id: cindy.id
+  )
+note5.save!
+
+note6 = Note.new(
+  content: "#{friend3.id} is a skincare fanatic",
+  friend_id: friend3.id,
+  user_id: cindy.id
+  )
+note6.save!
+
+puts "3 notes created"
+
+gift_idea4 = GiftIdea.new(
+  friend_id: friend3.id,
+  gift_id: gift4.id
+  )
+gift_idea4.save!
+
+gift_idea5 = GiftIdea.new(
+  friend_id: friend4.id,
+  gift_id: gift5.id
+  )
+gift_idea5.save!
+gift_idea6 = GiftIdea.new(
+  friend_id: friend4.id,
+  gift_id: gift3.id
+  )
+gift_idea6.save!
+
+puts "Gift ideas added to friends"
 
 
+puts "--"
+puts "
+*****************
 
+Login details are as follows:
+email: #{emily.email}
+email: #{cindy.email}
+password: password
+*****************
+"
 
-
-
-
-
-
-
-
-
+puts "--"
+puts 'Finished!'
 
 
 
