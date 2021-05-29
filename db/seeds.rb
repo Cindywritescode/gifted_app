cf1d0c4f961c06c18350502bcdeecb879d6a07e5
Emily Giblett <elgiblett@gmail.com>
2021-05-29 11:14:10 +0100

    seedfile started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all Users, gifts, friends, events, notes & gift ideas..."
+puts "Destroying all Users...."
+User.destroy_all
+puts "Destroying all Gifts...."
+Gift.destroy_all
+puts "Destroying all Friends (bye bitches 😉) ...."
+Friend.destroy_all
+puts "Destroying all Events...."
+Event.destroy_all
+puts "Destroying all Notes...."
+Note.destroy_all
+puts "Creating Users, gifts, friends, events, notes & gift ideas..."
+puts "--"
