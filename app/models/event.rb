54e4decb4035648257e9753b40590aa0d9cc123d
Thomas Loubser <taloubser@gmail.com>
2021-06-01 20:51:07 +0100

    added defined list for event type

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,11 @@ class Event < ApplicationRecord
   validates :friend_id, presence: true
   validates :event_type, presence: true
   validate :future_date
+  
+  TYPES = ['birthday', 'friendaversary', 'divorce party', 'graduation', 'baby shower', 'wedding']
+
+  validates :event_type, inclusion: { in: TYPES,
+    messae: "%{value} is not a valid event type" }
 
   def future_date
     errors.add(:date, "Cannot be in the past") if date < Date.today
