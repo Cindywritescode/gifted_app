a47dcf304f59de03daeb0aad6a32103e2e904637
Emily Giblett <elgiblett@gmail.com>
2021-06-03 20:32:13 +0100

    added scraping for etsy links

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 class GiftsController < ApplicationController
   before_action :set_gift, only: [:show, :edit, :update, :destroy]
 
@@ -8,12 +11,17 @@ class GiftsController < ApplicationController
   def create
     @gift = Gift.new(gift_params)
     @gift.user = current_user
+    if @gift.gift_url.include? 'etsy'
+      etsy_info(@gift.gift_url)
+      @gift.product_name = @info[0]
+      @gift.photo = @info[1]
+    end
     friend_id = @gift.friend_ids.first
     if @gift.save && !friend_id.nil?
       @gift_idea = GiftIdea.new(friend_id: friend_id, gift_id: @gift.id)
       @gift_idea.save!
       redirect_to friend_path(friend_id)
-    elsif @gift.save && friend_id.nil
+    elsif @gift.save && friend_id.nil?
       redirect_to gifts_path
     else
       render :new
@@ -23,7 +31,6 @@ class GiftsController < ApplicationController
   def index
     @gifts = Gift.where(user: current_user)
   end
-
 
   def destroy
     @gift.destroy
@@ -38,6 +45,17 @@ class GiftsController < ApplicationController
 
   def set_gift
     @gift = Gift.find(params[:id])
+  end
+
+  def etsy_info(gift_url)
+    url = gift_url
+    html_file = URI.open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    @info = []
+    title = html_doc.search('title').first.text
+    img = html_doc.search('img').first.attribute('src')
+    @info << title
+    @info << img.value
   end
 
 end
