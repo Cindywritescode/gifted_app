94fd4e2ff0a07fff80cd5234fd13d606f67f025b
Daniella Mogilner <daniella.m@hotmail.co.uk>
2021-06-05 14:16:03 +0100

    changed routes for event

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "events", to: "events#all", as: :all
+  get "events", to: "events#all"
 
   # # Sidekiq Web UI, only for admins.
   # require "sidekiq/web"
