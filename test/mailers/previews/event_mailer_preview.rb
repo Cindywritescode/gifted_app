ec4af6824d7f1a641b305dd08f864d92a634813f
Thomas Loubser <taloubser@gmail.com>
2021-06-20 15:35:48 +0100

    Updated Email CSS

--- test/mailers/previews/event_mailer_preview.rb
+++ test/mailers/previews/event_mailer_preview.rb
@@ -3,7 +3,7 @@ class EventMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/event_mailer/notify14
   def notify14
-    EventMailer.notify14
+    EventMailer.notify14(Event.last)
   end
 
 end
