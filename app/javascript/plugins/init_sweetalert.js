e48ed2b266cb32bf3dcaca51da895b0cbd29a915
Thomas Loubser <taloubser@gmail.com>
2021-06-24 22:58:12 +0100

    Added event listener to all ids

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -2,23 +2,25 @@
 import swal from 'sweetalert';
 
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) {
-    swalButton.addEventListener('click', () => {
-      const name = swalButton.getAttribute("data-friend-name");
-      const id = swalButton.getAttribute('data-friend-id');
-      swal({
-        title: "We fell out 🤬",
-        text: "No more thoughtful presents for " + name + "!!!",
-        buttons: [ "Cancel", true ],
-        dangerMode: true,
-        closeModal: true,
-      }).then(value => {
-        if (value) {
-        const link = document.querySelector("#delete-" + id);
-        link.click();
-        }
-      })
+  const swalButtons = document.querySelectorAll(selector);
+  if (swalButtons.length > 0) {
+    swalButtons.forEach(swalButton => {
+      swalButton.addEventListener('click', () => {
+        const name = swalButton.getAttribute("data-friend-name");
+        const id = swalButton.getAttribute('data-friend-id');
+        swal({
+          title: "We fell out 🤬",
+          text: "No more thoughtful presents for " + name + "!!!",
+          buttons: [ "Cancel", true ],
+          dangerMode: true,
+          closeModal: true,
+        }).then(value => {
+          if (value) {
+          const link = document.querySelector("#delete-" + id);
+          link.click();
+          }
+        })
+      });
     });
   }
 };
